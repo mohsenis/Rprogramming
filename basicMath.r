10d025c318ffcb099103e670977a60e27ca788ee
Alireza Mohseni <mohseni.s.alireza@gmail.com>
2016-09-06 12:01:13 -0700

    fourth commit

---
+++ basicMath.r
@@ -0,0 +1,14 @@
+4/3  #in r, this division gives you the actual decimal number
+
+x <- 2
+x
+
+a <- b <- 7
+a
+b
+
+assign("i", 4)
+i
+
+rm(i)
+i
--- first.r
+++ first.r
@@ -1,1 +1,7 @@
 1+1
+
+library(ggplot2)
+require(coefplot)  #kind of the same as library
+
+install.packages("useful")
+require(useful)
